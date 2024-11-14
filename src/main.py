a1e144e8dbc0774c9caed4fa84547525c4ef4bc0
JaimeCorrales <jaime.corralesdelgado@alum.uca.es>
2024-11-14 21:24:40 +0100

    log

--- src/main.py
+++ src/main.py
@@ -2,8 +2,17 @@ import time
 from config import Config
 from log_setup import setup_logging
 from client import FootballStatsClient
+import logging
 
 def main():
+    
+    logging.basicConfig(
+    filename='app.log',          
+    level=logging.DEBUG,         
+    format='%(asctime)s - %(levelname)s - %(message)s',  
+    datefmt='%Y-%m-%d %H:%M:%S'   
+)
+    
     # Load configuration from the YAML file
     config = Config(config_file="config.yaml")
     logger = setup_logging(log_level=config.get("log_level"), log_file=config.get("log_file"))
